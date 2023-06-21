6ee585ea84ab22be7a84f3c5e1e66c7bfce16c36
abhishek-goutam <abhishekgoutam8894@gmail.com>
2023-06-21 10:56:03 +0530

    Backend for Signup with use cases like user exist added

--- controller/expense.controller.js
+++ controller/expense.controller.js
@@ -1,4 +1,5 @@
 const Expenses = require("../model/expense.model");
+const User = require("../model/users.model");
 
 module.exports = {
   getAllExpense: async (req, res) => {
@@ -29,24 +30,33 @@ module.exports = {
       .catch((err) => res.send(err));
   },
 
-
   deleteExpense: async (req, res) => {
     try {
       const data = await Expenses.destroy({ where: { id: req.params.id } });
-      console.log("Deleted",data)
+      console.log("Deleted", data);
       res.send("data");
     } catch (error) {
       console.log(error);
     }
   },
 
-  signUp : async(req,res)=>{
-    try {
-      
-    } catch (error) {
-      
+  signUp: async (req, res) => {
+    const email = req.body.email;
+
+    const findUser = await User.findOne({ where: { email } });
+
+    if (!findUser) {
+      User.build(req.body)
+        .save()
+        .then((data) => {
+          res.send(data);
+        })
+        .catch((err) => res.send(err));
+    } else {
+      console.log("User Already exist");
+      res.send("User exists");
     }
-  }
+
+    // const findUser =  await User.findOne({where:{req.body.email}})
+  },
 };
-
-
---
+++ util/signUp.js
@@ -0,0 +1,19 @@
+async function signUp() {
+  try {
+    const data = {
+      name: document.getElementById("form3Example1c").value,
+      email: document.getElementById("form3Example3c").value,
+      password: document.getElementById("form3Example4c").value,
+    };
+    const res = await axios.post(
+      "http://localhost:3000/api/expense/signUp",
+      data
+    );
+    // console.log("Message",res.message);
+    document.getElementById("addToMe").innerHTML +=
+    "<h3>User already exist</h3>";
+    // window.alert(res);
+  } catch (error) {
+    console.log(error);
+  }
+}
